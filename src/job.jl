4cb61f6abcef75f409e510b5cdb88c1999fe04b7
Rasmus Henningsson <rasmus.henningsson@gmail.com>
2024-10-09 14:48:51 +0200

    Store result in Job

--- src/job.jl
+++ src/job.jl
@@ -1,6 +1,23 @@
+struct NotComputed end
+
 mutable struct Job
 	const spec::Spec
-	# TODO: maybe status and value, or maybe the handle is used for that.
+	result::Any
+end
+Job(spec::Spec) = Job(spec, NotComputed())
+
+function fetch!(job; scheduler=default_scheduler())
+	job.result = fetch!(scheduler, job.spec)
 end
 
-fetch!(job; scheduler=default_scheduler()) = fetch!(scheduler, job.spec)
+# --- printing ---
+function Base.show(io::IO, ::MIME"text/plain", job::Job)
+	println(io, "Job Spec:")
+	show(io, MIME"text/plain"(), job.spec)
+	# println(io)
+	print(io, "Job Result: ")
+	let io = IOContext(io, :compact=>true)
+		show(io, job.result)
+	end
+	println(io)
+end
