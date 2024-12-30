f8a8cbb3837a8c81a3d77287d450d25726cf8823
Rasmus Henningsson <rasmus.henningsson@gmail.com>
2024-12-30 09:46:51 +0800

    Getters etc. for Managed containers

--- src/managed.jl
+++ src/managed.jl
@@ -7,15 +7,12 @@ end
 manage(x) = Managed(x)
 manage(x::Managed) = x
 
-# TODO: handle basic types like Int etc
+# Basic immutable types - should this list be extended?
+manage(x::Union{Number,String,Symbol,Nothing,Missing}) = x
 
 
 # Anyone using this must treat the object (and any nested objects within) as read-only
 unsafe_unmanage(m::Managed) = m.x
-
-
-# How to do nested unmanage properly?
-# unmanage(m::Managed{<:Array}) = ReadOnlyArray(m.x) # not enough, doesn't handle nesting
 
 
 unmanage(x) = x
@@ -28,15 +25,38 @@ unmanage(m::Managed{<:ReadOnly{T}}) where {T<:Array} = ReadOnlyArray(m.x.value)
 
 unmanage(m::Managed{<:Dict}) = Dict(unmanage(k)=>unmanage(v) for (k,v) in m.x)
 unmanage(m::Managed{<:Set}) = Set(unmanage(a) for a in m.x)
+# TODO: ReadOnlyDict and ReadOnlySet if stored in ReadOnly
 
 unmanage(m::Managed{<:Pair}) = unmanage(m.x.first)=>unmanage(m.x.second)
 unmanage(m::Managed{<:Tuple}) = unmanage.(m.x)
 unmanage(m::Managed{<:NamedTuple}) = map(unmanage, m.x)
 
-# supported containers
-Base.getindex(m::Managed{<:AbstractArray}, args...) =
+# Getters that wrap items as they are returned
+Base.getindex(m::Managed{<:Union{Array,Dict,Pair,Tuple,NamedTuple}}, args...) =
 	manage.(getindex(m.x, args...))
-Base.getindex(m::Managed{<:Tuple}) =
-	manage.(getindex(m.x, args...))
-# TODO: more functions and support for pair, dict, set, namedtuple
+Base.get(m::Managed{<:Union{Array,Dict,Tuple,NamedTuple}}, args...) =
+	manage.(get(m.x, args...))
 
+Base.getindex(m::Managed{<:ReadOnly{T}}, args...) where {T<:Union{Array,Dict}} =
+	getindex(m.x.value, args...)
+Base.get(m::Managed{<:ReadOnly{T}}, args...) where {T<:Union{Array,Dict}} =
+	get(m.x.value, args...)
+
+# Forward array/dict stuff
+Base.length(m::Managed{<:T}) where {T<:Union{Array,Dict,Set,Pair,Tuple,NamedTuple}} = length(m.x)
+Base.length(m::Managed{<:ReadOnly{T}}) where {T<:Union{Array,Dict,Set}} = length(m.x.value)
+
+Base.size(m::Managed{<:Array}, args...) = size(m.x, args...)
+Base.size(m::Managed{<:ReadOnly{<:Array}}, args...) = size(m.x.value, args...)
+
+Base.firstindex(m::Managed{<:T}) where {T<:Union{Array,Pair,Tuple,NamedTuple}} = firstindex(m.x)
+Base.firstindex(m::Managed{<:ReadOnly{<:Array}}) = firstindex(m.x.value)
+Base.lastindex(m::Managed{<:T}) where {T<:Union{Array,Pair,Tuple,NamedTuple}} = lastindex(m.x)
+Base.lastindex(m::Managed{<:ReadOnly{<:Array}}) = lastindex(m.x.value)
+
+Base.keys(m::Managed{<:T}) where {T<:Union{Array,Pair,Tuple,NamedTuple}} = keys(m.x)
+Base.keys(m::Managed{<:ReadOnly{<:Array}}) = keys(m.x.value)
+
+# The keys in a Dict might be managed, so we need to call manage here.
+Base.keys(m::Managed{<:Dict}) = manage.(keys(m.x))
+Base.keys(m::Managed{<:ReadOnly{<:Dict}}) = manage.(keys(m.x.value))
