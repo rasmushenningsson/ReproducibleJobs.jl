7ae658948c57a247236f6d0b32cbf4798f13a96f
Rasmus Henningsson <rasmus.henningsson@gmail.com>
2026-05-11 16:12:54 +0200

    Reordered file contents

--- src/spec.jl
+++ src/spec.jl
@@ -9,26 +9,6 @@ struct Spec
 	end
 end
 
-# Usually accessed through getproperty
-get_function(spec::Spec) = spec.f
-get_args(spec::Spec) = spec.args
-get_kwargs(spec::Spec) = spec.kwargs
-
-
-function _find_kwarg(key::Symbol, kw::Vector{Pair{Symbol,Any}})
-	for (i,(k,_)) in enumerate(kw)
-		k == key && return i
-	end
-	return nothing
-end
-function _get_kwarg(f, spec::Spec, key::Symbol)
-	i = _find_kwarg(key, spec.kwargs)
-	i === nothing ? f() : spec.kwargs[i].second
-end
-_get_kwarg(spec::Spec, key::Symbol, default) = _get_kwarg(()->default, spec, key)
-_get_kwarg(spec::Spec, key::Symbol) = _get_kwarg(()->throw(KeyError(key)), spec, key)
-
-
 
 mutable struct SpecRun{T}
 	const spec::Spec
@@ -46,6 +26,12 @@ struct SpecRef{T}
 	end
 end
 SpecRef(sr::SpecRun{T}, op=:forward) where T = SpecRef{T}(sr, op)
+
+
+
+Base.Broadcast.broadcastable(spec::Spec) = Ref(spec) # treat as scalar for broadcasting
+Base.Broadcast.broadcastable(sr::SpecRun) = Ref(sr) # treat as scalar for broadcasting
+Base.Broadcast.broadcastable(ref::SpecRef) = Ref(ref) # treat as scalar for broadcasting
 
 
 
@@ -112,155 +98,67 @@ SpecRun(spec::Spec) = SpecRun(spec, state_initialized())
 
 
 
+function create_spec(f, args...; scheduler=get_scheduler(), deduplicator=scheduler.deduplicator, kwargs...)
+	a = collect(Any, args)
+	kw = collect(Pair{Symbol,Any}, kwargs)
+	sort!(kw; by=first)
+	sr = SpecRun(Spec(f, a, kw))
 
+	deduplicator !== nothing && (sr = deduplicate!(deduplicator, sr))
+	SpecRef(sr)
+end
 
 
 
 
 get_spec(sr::SpecRun) = sr.spec
+get_spec(ref::SpecRef) = get_spec(ref.sr)
+get_sr(ref::SpecRef) = ref.sr
+
+
 
 # Usually accessed through getproperty
-get_function(sr::SpecRun) = get_function(sr.spec)
-get_args(sr::SpecRun) = get_args(sr.spec)
-get_kwargs(sr::SpecRun) = get_kwargs(sr.spec)
+get_function(spec::Spec) = spec.f
+get_args(spec::Spec) = spec.args
+get_kwargs(spec::Spec) = spec.kwargs
 
-function Base.getproperty(sr::SpecRun, s::Symbol)
-	s === :f && return get_function(sr)
-	s === :args && return get_args(sr)
-	s === :kwargs && return get_kwargs(sr)
-	getfield(sr, s)
+get_function(x::Union{SpecRun,SpecRef}) = get_function(get_spec(x))
+get_args(x::Union{SpecRun,SpecRef}) = get_args(get_spec(x))
+get_kwargs(x::Union{SpecRun,SpecRef}) = get_kwargs(get_spec(x))
+
+
+function Base.getproperty(x::Union{SpecRun,SpecRef}, s::Symbol)
+	s === :f && return get_function(x)
+	s === :args && return get_args(x)
+	s === :kwargs && return get_kwargs(x)
+	getfield(x, s)
 end
 Base.propertynames(::SpecRun, private::Bool=false) =
 	private ? (:f, :args, :kwargs, :spec, :state) : (:f, :args, :kwargs)
+Base.propertynames(::SpecRef, private::Bool=false) =
+	private ? (:f, :args, :kwargs, :op, :sr) : (:f, :args, :kwargs, :op)
 
 
 
-# Doesn't create a new vector unless any child changed during deduplication
-function _map_arg_vec(f, a::Vector{T}) where T
-	out = a
-	for (i,x) in enumerate(a)
-		fx = f(x)
-		if fx !== x
-			if out === a
-				out = copy(a)
-			end
-			out[i] = fx
-		end
+
+
+
+function _find_kwarg(key::Symbol, kw::Vector{Pair{Symbol,Any}})
+	for (i,(k,_)) in enumerate(kw)
+		k == key && return i
 	end
-	out
+	return nothing
 end
-
-
-
-
-deduplicate_type(::Type{<:SpecRun}) = true
-deduplication_pointer(sr::SpecRun) = pointer_from_objref(sr)
-function deduplicate_children!(d, sr::SpecRun; kwargs...)
-	f = sr.f # TODO: Should this be processed somehow? Probably not.
-
-	a = _map_arg_vec(sr.args) do x
-		deduplicate!(d, x; kwargs...)
-	end
-	kw = _map_arg_vec(sr.kwargs) do p
-		p[1]=>deduplicate!(d, p[2]; kwargs...)
-	end
-
-	if f === sr.f && a === sr.args && kw === sr.kwargs
-		sr # Not changed
-	else
-		SpecRun(Spec(f, a, kw))
-	end
+function _get_kwarg(f, spec::Spec, key::Symbol)
+	i = _find_kwarg(key, spec.kwargs)
+	i === nothing ? f() : spec.kwargs[i].second
 end
-function deduplication_hash(d, sr::SpecRun)
-	# TODO: Could we make this more efficient? (Is it a problem? Probably not.)
+_get_kwarg(spec::Spec, key::Symbol, default) = _get_kwarg(()->default, spec, key)
+_get_kwarg(spec::Spec, key::Symbol) = _get_kwarg(()->throw(KeyError(key)), spec, key)
 
-	# Tuple/NamedTuple version
-	# f = sr.f
-	# a = deduplication_hash(d, sr.args)
-	# kw = deduplication_hash(d, sr.kwargs)
-	# compute_hash(d, (TypeTag(:SpecRun), f, a, kw))
-
-	# Vector{Any}/Vector{Pair{Symbol,Any}} version
-	a = _map_arg_vec(sr.args) do x
-		hash_or_value(d, x)
-	end
-	kw = _map_arg_vec(sr.kwargs) do p
-		p[1]=>hash_or_value(d, p[2])
-	end
-	compute_hash(d, (TypeTag(:Spec), sr.f, a, kw))
-end
-deduplication_copy(sr::SpecRun) = sr
-
-
-# TODO: THIS ISN'T GOOD ENOUGH! WE NEED TO ENSURE WE ONLY STORE EACH SPEC ONCE, EVEN IF IT'S REFERENCED MULTIPLE TIMES
-function cache_save(io, name, sr::SpecRun)
-	# Save as a group
-	g = JLD2.Group(io, name)
-	g["type"] = "Spec"
-	g["f"] = sr.f # Is this the best I can do?
-	cache_save(g, "args", sr.args) # TODO: Must be fixed now that we use Vector{Any}
-	cache_save(g, "kwargs", sr.kwargs) # TODO: Must be fixed now that we use Vector{Pair{Symbol,Any}}
-	nothing
-end
-function cache_load(cache::Cache, ::Val{:Spec}, g)
-	f = g["f"]
-	args = cache_load(cache, g, "args") # TODO: Must be fixed now that we use Vector{Any}
-	kwargs = cache_load(cache, g, "kwargs") # TODO: Must be fixed now that we use Vector{Pair{Symbol,Any}}
-	sr = SpecRun(Spec(f, args, kwargs))
-	deduplicate!(cache.deduplicator, sr; transfer_ownership=true)
-end
-
-
-
-
-function sa_isequal(a::SpecRun, b::SpecRun)
-	a === b && return true # early out
-	isequal(a.f, b.f) && sa_isequal(a.args, b.args) && sa_isequal(a.kwargs, b.kwargs)
-end
-
-function sa_isequal(a::AbstractVector{T1}, b::AbstractVector{T2}) where {T1,T2}
-	isequal(length(a), length(b)) || return false
-	all(t->sa_isequal(t[1], t[2]), zip(a,b))
-end
-
-function sa_isequal(a::Dict{K1,V1}, b::Dict{K2,V2}) where {K1,V1,K2,V2}
-	isequal(length(a), length(b)) || return false
-    for pair in a
-        in(pair, b, sa_isequal) || return false
-    end
-    true
-end
-
-sa_isequal(a::NamedTuple{n}, b::NamedTuple{n}) where n = sa_isequal(Tuple(a), Tuple(b))
-sa_isequal(a::NamedTuple, b::NamedTuple) = false # keys are different
-
-function sa_isequal(a::DataFrame, b::DataFrame)
-	isequal(ncol(a), ncol(b)) || return false
-	isequal(names(a), names(b)) || return false
-	all(t->sa_isequal(t[1], t[2]), zip(eachcol(a),eachcol(b)))
-end
-
-function sa_isequal(a::T1, b::T2) where {T1,T2}
-	if deconstruct_type(T1) && deconstruct_type(T2)
-		ad = deconstruct(a)::Tuple
-		bd = deconstruct(b)::Tuple
-
-		isequal(length(ad), length(bd)) || return false
-		all(t->sa_isequal(t[1], t[2]), zip(ad,bd))
-	else
-		# TODO: Should we have this fallback?
-		isequal(a,b)
-	end
-end
-
-Base.isequal(a::SpecRun, b::SpecRun) = sa_isequal(a, b)
-
-
-
-
-_get_kwarg(f, sr::SpecRun, key::Symbol) = _get_kwarg(f, get_spec(sr), key)
-_get_kwarg(sr::SpecRun, key::Symbol, default) = _get_kwarg(get_spec(sr), key, default)
-_get_kwarg(sr::SpecRun, key::Symbol) = _get_kwarg(get_spec(sr), key)
+_get_kwarg(f, x::Union{SpecRun,SpecRef}, key::Symbol) = _get_kwarg(f, get_spec(x), key)
+_get_kwarg(x::Union{SpecRun,SpecRef}, key::Symbol, default) = _get_kwarg(get_spec(x), key, default)
+_get_kwarg(x::Union{SpecRun,SpecRef}, key::Symbol) = _get_kwarg(get_spec(x), key)
 
 
 
@@ -307,46 +205,6 @@ end
 
 
 
-Base.Broadcast.broadcastable(spec::Spec) = Ref(spec) # treat as scalar for broadcasting
-Base.Broadcast.broadcastable(sr::SpecRun) = Ref(sr) # treat as scalar for broadcasting
-Base.Broadcast.broadcastable(ref::SpecRef) = Ref(ref) # treat as scalar for broadcasting
-
-
-
-
-
-
-
-
-
-get_spec(ref::SpecRef) = get_spec(ref.sr)
-get_sr(ref::SpecRef) = ref.sr
-SpecRun(ref::SpecRef) = get_sr(ref)
-
-
-
-# Usually accessed through getproperty
-get_function(ref::SpecRef) = get_function(get_spec(ref))
-get_args(ref::SpecRef) = get_args(get_spec(ref))
-get_kwargs(ref::SpecRef) = get_kwargs(get_spec(ref))
-
-function Base.getproperty(ref::SpecRef, s::Symbol)
-	s === :f && return get_function(ref)
-	s === :args && return get_args(ref)
-	s === :kwargs && return get_kwargs(ref)
-	getfield(ref, s)
-end
-Base.propertynames(::SpecRef, private::Bool=false) =
-	private ? (:f, :args, :kwargs, :sr) : (:f, :args, :kwargs)
-
-
-
-_get_kwarg(f, ref::SpecRef, key::Symbol) = _get_kwarg(f, get_spec(ref), key)
-_get_kwarg(ref::SpecRef, key::Symbol, default) = _get_kwarg(get_spec(ref), key, default)
-_get_kwarg(ref::SpecRef, key::Symbol) = _get_kwarg(get_spec(ref), key)
-
-
-
 function transfer_op(src::SpecRef, dest::SpecRef)
 	if src.op == :call
 		dest.op === :call ? dest : SpecRef(get_sr(dest), :forward) # We can keep Call, but never transfer it (so fallback to standard forwarding)
@@ -370,35 +228,6 @@ try_get_result_rec(ref::SpecRef) = try_get_result_rec(get_sr(ref))
 
 
 
-
-deduplicate_type(::Type{Job}) = true
-deconstruct_type(::Type{Job}) = true
-type_to_tag(::Type{Job}) = TypeTag(:Job)
-tag_to_type(::Val{:Job}) = Job
-deconstruct(ref::Job) = (ref.sr, ref.op)
-reconstruct(::Type{Job}, (sr,op)::Tuple{SpecRun{State},Symbol}) = Job(sr, op)
-
-
-
-
-function create_spec(f, args...; scheduler=get_scheduler(), deduplicator=scheduler.deduplicator, kwargs...)
-	# Tuple/NamedTuple version
-	# kw = values(kwargs)
-	# kw = sort_namedtuple_by_keys(kw)
-	# sr = SpecRun(f, args, kw)
-
-	# Vector{Any}/Vector{Pair{Symbol,Any}} version
-	a = collect(Any, args)
-	kw = collect(Pair{Symbol,Any}, kwargs)
-	sort!(kw; by=first)
-	sr = SpecRun(Spec(f, a, kw))
-
-	deduplicator !== nothing && (sr = deduplicate!(deduplicator, sr))
-	SpecRef(sr)
-end
-
-
-Base.isequal(a::SpecRef, b::SpecRef) = isequal(a.op, b.op) && isequal(a.sr, b.sr)
 
 
 
@@ -446,6 +275,24 @@ function visit_specs(f, x::T) where T
 		x
 	end
 end
+
+
+
+# Doesn't create a new vector unless any child changed during deduplication
+function _map_arg_vec(f, a::Vector{T}) where T
+	out = a
+	for (i,x) in enumerate(a)
+		fx = f(x)
+		if fx !== x
+			if out === a
+				out = copy(a)
+			end
+			out[i] = fx
+		end
+	end
+	out
+end
+
 
 
 # Find a better name?
@@ -525,6 +372,137 @@ _prefetched(ref::SpecRef) = SpecRef(ref.sr, :prefetch)
 
 fetched(x) = map_specs(_fetched, x)
 prefetched(x) = map_specs(_prefetched, x)
+
+
+
+
+
+
+function spec_isequal(a::SpecRun, b::SpecRun)
+	a === b && return true # early out
+	spec_isequal(a.spec, b.spec)
+end
+
+spec_isequal(a::Spec, b::Spec) =
+	isequal(a.f, b.f) && spec_isequal(a.args, b.args) && spec_isequal(a.kwargs, b.kwargs)
+
+function spec_isequal(a::AbstractVector{T1}, b::AbstractVector{T2}) where {T1,T2}
+	isequal(length(a), length(b)) || return false
+	all(t->spec_isequal(t[1], t[2]), zip(a,b))
+end
+
+function spec_isequal(a::Dict{K1,V1}, b::Dict{K2,V2}) where {K1,V1,K2,V2}
+	isequal(length(a), length(b)) || return false
+    for pair in a
+        in(pair, b, spec_isequal) || return false
+    end
+    true
+end
+
+spec_isequal(a::NamedTuple{n}, b::NamedTuple{n}) where n = spec_isequal(Tuple(a), Tuple(b))
+spec_isequal(a::NamedTuple, b::NamedTuple) = false # keys are different
+
+function spec_isequal(a::DataFrame, b::DataFrame)
+	isequal(ncol(a), ncol(b)) || return false
+	isequal(names(a), names(b)) || return false
+	all(t->spec_isequal(t[1], t[2]), zip(eachcol(a),eachcol(b)))
+end
+
+function spec_isequal(a::T1, b::T2) where {T1,T2}
+	if deconstruct_type(T1) && deconstruct_type(T2)
+		ad = deconstruct(a)::Tuple
+		bd = deconstruct(b)::Tuple
+
+		isequal(length(ad), length(bd)) || return false
+		all(t->spec_isequal(t[1], t[2]), zip(ad,bd))
+	else
+		# TODO: Should we have this fallback?
+		isequal(a,b)
+	end
+end
+
+Base.isequal(a::SpecRun, b::SpecRun) = spec_isequal(a, b)
+Base.isequal(a::SpecRef, b::SpecRef) = isequal(a.op, b.op) && isequal(a.sr, b.sr)
+
+
+
+
+
+
+
+
+deduplicate_type(::Type{<:SpecRun}) = true
+deduplication_pointer(sr::SpecRun) = pointer_from_objref(sr)
+function deduplicate_children!(d, sr::SpecRun; kwargs...)
+	f = sr.f # TODO: Should this be processed somehow? Probably not.
+
+	a = _map_arg_vec(sr.args) do x
+		deduplicate!(d, x; kwargs...)
+	end
+	kw = _map_arg_vec(sr.kwargs) do p
+		p[1]=>deduplicate!(d, p[2]; kwargs...)
+	end
+
+	if f === sr.f && a === sr.args && kw === sr.kwargs
+		sr # Not changed
+	else
+		SpecRun(Spec(f, a, kw))
+	end
+end
+function deduplication_hash(d, sr::SpecRun)
+	# TODO: Could we make this more efficient? (Is it a problem? Probably not.)
+
+	# Tuple/NamedTuple version
+	# f = sr.f
+	# a = deduplication_hash(d, sr.args)
+	# kw = deduplication_hash(d, sr.kwargs)
+	# compute_hash(d, (TypeTag(:SpecRun), f, a, kw))
+
+	# Vector{Any}/Vector{Pair{Symbol,Any}} version
+	a = _map_arg_vec(sr.args) do x
+		hash_or_value(d, x)
+	end
+	kw = _map_arg_vec(sr.kwargs) do p
+		p[1]=>hash_or_value(d, p[2])
+	end
+	compute_hash(d, (TypeTag(:Spec), sr.f, a, kw))
+end
+deduplication_copy(sr::SpecRun) = sr
+
+
+# TODO: THIS ISN'T GOOD ENOUGH! WE NEED TO ENSURE WE ONLY STORE EACH SPEC ONCE, EVEN IF IT'S REFERENCED MULTIPLE TIMES
+function cache_save(io, name, sr::SpecRun)
+	# Save as a group
+	g = JLD2.Group(io, name)
+	g["type"] = "Spec"
+	g["f"] = sr.f # Is this the best I can do?
+	cache_save(g, "args", sr.args) # TODO: Must be fixed now that we use Vector{Any}
+	cache_save(g, "kwargs", sr.kwargs) # TODO: Must be fixed now that we use Vector{Pair{Symbol,Any}}
+	nothing
+end
+function cache_load(cache::Cache, ::Val{:Spec}, g)
+	f = g["f"]
+	args = cache_load(cache, g, "args") # TODO: Must be fixed now that we use Vector{Any}
+	kwargs = cache_load(cache, g, "kwargs") # TODO: Must be fixed now that we use Vector{Pair{Symbol,Any}}
+	sr = SpecRun(Spec(f, args, kwargs))
+	deduplicate!(cache.deduplicator, sr; transfer_ownership=true)
+end
+
+
+
+
+
+
+deduplicate_type(::Type{Job}) = true
+deconstruct_type(::Type{Job}) = true
+type_to_tag(::Type{Job}) = TypeTag(:Job)
+tag_to_type(::Val{:Job}) = Job
+deconstruct(ref::Job) = (ref.sr, ref.op)
+reconstruct(::Type{Job}, (sr,op)::Tuple{SpecRun{State},Symbol}) = Job(sr, op)
+
+
+
+
 
 
 
